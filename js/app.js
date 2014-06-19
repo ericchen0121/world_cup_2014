c5d8344fbcf7f9456490b4434643bc0f544c1d15
Eric Savvy <eric.chen0121@gmail.com>
2014-06-19 15:08:29 -0700

    remove bootstrap 2 and add bootstrap 3 responsive styles

--- js/app.js
+++ js/app.js
@@ -11,24 +11,31 @@ App.TeamsRoute = Ember.Route.extend({
   }
 });
 
-/* fixture data */
-var teams = $.getJSON("http://worldcup.kimonolabs.com/api/teams?apikey=ed489eaaa82064ee89efa4fc4efcf42f", function(data){ return data;});
+/* World Cup API json data */
+var teams = $.getJSON("http://worldcup.kimonolabs.com/api/teams?sort=name&apikey=ed489eaaa82064ee89efa4fc4efcf42f", function(data){ return data;});
 
 
+/* Example World Cup API json response
 
-/*[{
-  id: '1',
-  team: 'Brazil',
-  group: 'A',
-  wins: 1,
-  losses: 0,
-  ties: 1
-},
-{
-  id: '2',
-  team: 'United States',
-  group: 'G',
-  wins: 1,
-  losses: 0,
-  ties: 1
-}];*/
+[
+  {
+    "name": "Costa Rica",
+    "logo": "http://cache.images.globalsportsmedia.com/soccer/teams/150x150/477.png",
+    "website": "http://www.fedefutbol.com",
+    "foundedYear": 1921,
+    "address": "Radial Santa Ana Belén\n670-1000\nSan José",
+    "homeStadium": "Estadio Nacional de Costa Rica",
+    "stadiumCapacity": 35093,
+    "group": "D",
+    "groupRank": 1,
+    "groupPoints": 3,
+    "matchesPlayed": 1,
+    "wins": 1,
+    "losses": 0,
+    "draws": 0,
+    "goalsFor": 3,
+    "goalsAgainst": 1,
+    "goalsDiff": "+2",
+    "id": "F77B348A-D7AE-4534-8ADA-8E52BEE64744",
+    "type": "Team"
+  }, ...] */
