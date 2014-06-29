fce5b7ce43cb72af11e199a5a8e3472f6fa774cb
Eric Savvy <eric.chen0121@gmail.com>
2014-06-28 19:18:12 -0700

    remove sample api response which was commented out from js file

--- js/app.js
+++ js/app.js
@@ -411,28 +411,3 @@ var teamId = [
     "id": "088C4113-CEFC-460C-830C-277C148C0CE7"
   }
 ]
-
-/* Example World Cup API json response
-
-[
-  {
-    "name": "Costa Rica",
-    "logo": "http://cache.images.globalsportsmedia.com/soccer/teams/150x150/477.png",
-    "website": "http://www.fedefutbol.com",
-    "foundedYear": 1921,
-    "address": "Radial Santa Ana Belén\n670-1000\nSan José",
-    "homeStadium": "Estadio Nacional de Costa Rica",
-    "stadiumCapacity": 35093,
-    "group": "D",
-    "groupRank": 1,
-    "groupPoints": 3,
-    "matchesPlayed": 1,
-    "wins": 1,
-    "losses": 0,
-    "draws": 0,
-    "goalsFor": 3,
-    "goalsAgainst": 1,
-    "goalsDiff": "+2",
-    "id": "F77B348A-D7AE-4534-8ADA-8E52BEE64744",
-    "type": "Team"
-  }, ...] */
