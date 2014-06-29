0e64c5bdeb7c4f246a0e2388e06e1680ebef2f87
Eric Savvy <eric.chen0121@gmail.com>
2014-06-28 19:16:53 -0700

    remove compare js script, not using it in template

--- js/app.js
+++ js/app.js
@@ -97,38 +97,6 @@ App.Router.reopen({
   }.on('didTransition')
 });
 
-/* http://doginthehat.com.au/2012/02/comparison-block-helper-for-handlebars-templates/ */
-
-Handlebars.registerHelper('compare', function(lvalue, rvalue, options) {
-
-    if (arguments.length < 3)
-        throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
-
-    operator = options.hash.operator || "==";
-
-    var operators = {
-        '==':       function(l,r) { return l == r; },
-        '===':      function(l,r) { return l === r; },
-        '!=':       function(l,r) { return l != r; },
-        '<':        function(l,r) { return l < r; },
-        '>':        function(l,r) { return l > r; },
-        '<=':       function(l,r) { return l <= r; },
-        '>=':       function(l,r) { return l >= r; },
-        'typeof':   function(l,r) { return typeof l == r; }
-    }
-
-    if (!operators[operator])
-        throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
-
-    var result = operators[operator](lvalue,rvalue);
-
-    if( result ) {
-        return options.fn(this);
-    } else {
-        return options.inverse(this);
-    }
-
-});
 
 /* World Cup API json data */
 var worldcupapi = "http://worldcup.kimonolabs.com/api/";
